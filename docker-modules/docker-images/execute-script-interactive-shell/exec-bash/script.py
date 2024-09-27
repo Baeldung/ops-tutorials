dcdbb923d60336e8d086147bef0c94f4820977ec
luca <lucacambi77@gmail.com>
2024-09-27 10:43:57 +0200

    fix: use only one script

--- docker-modules/docker-images/execute-script-interactive-shell/exec-bash/script.py
+++
@@ -1,6 +0,0 @@
-# script.py
-
-# Add any changes here
-# For example, creating a file in the container
-with open("/app/output.txt", "w") as f:
-    f.write("This file was created by script.py inside the Docker container.\n")
