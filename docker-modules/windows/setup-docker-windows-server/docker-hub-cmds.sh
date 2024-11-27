38b93a096732c0104b22f5c3616822d6860adfeb
dvohra09 <dvohra09@yahoo.com>
2024-11-27 12:46:22 -0800

    Create docker-hub-cmds.sh

---
+++ docker-modules/windows/setup-docker-windows-server/docker-hub-cmds.sh
@@ -0,0 +1,15 @@
+docker login [OPTIONS] [SERVER]
+
+docker login -u username -p password
+
+docker image tag helloworld dvohra/helloworld
+
+docker images
+
+docker push dvohra/helloworld
+
+docker rmi dvohra/helloworld
+
+docker pull dvohra/helloworld
+
+docker run --rm dvohra/helloworld cmd.exe /s /c echo "Hello!"
