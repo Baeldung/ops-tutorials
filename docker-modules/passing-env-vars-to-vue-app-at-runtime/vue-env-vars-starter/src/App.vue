523c1035b52d9d49ad18ed7b65bf2a96827b305e
davydocsurg <davydocsurg@gmail.com>
2025-01-23 21:33:05 +0100

    update passing env vars to Vue app

---
+++ docker-modules/passing-env-vars-to-vue-app-at-runtime/vue-env-vars-starter/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <CurrencyConverter />
+    </div>
+</template>
+
+<script>
+    import CurrencyConverter from "./components/CurrencyConverter.vue";
+    export default {
+        components: {
+            CurrencyConverter,
+        },
+    };
+</script>
+
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+</style>
